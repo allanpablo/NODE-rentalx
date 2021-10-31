ef9843f4b77a5ac19967a4051f82e7a2d0a3960f
Allan Pablo <allanpablo@gmail.com>
2021-10-31 15:35:17 -0300

    up

--- tests/custom_resolver.ts
+++
@@ -1,6 +0,0 @@
-module.exports = (request, options) => {
-  if (request.search(/nodemailer/gi) > -1) {
-    return `${options.rootDir}/mocks/nodemailer.ts`;
-  }
-  return options.defaultResolver(request, options);
-};
